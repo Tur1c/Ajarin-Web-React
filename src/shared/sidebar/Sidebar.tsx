5867a57bb917c4c180b1363cbddda674225d9aa8
Ivander Gladwin <ivanderglad200@gmail.com>
2024-01-27 19:15:19 +0700

    update logo + css

--- src/shared/sidebar/Sidebar.tsx
+++ src/shared/sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react";
-import { MdMenu } from "react-icons/md";
 import { GoHome } from "react-icons/go";
 import { HiOutlineBookOpen } from "react-icons/hi2";
-import { PiEyeglassesLight } from "react-icons/pi";
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { MdMenu } from "react-icons/md";
+import { PiEyeglassesLight } from "react-icons/pi";
 import "./Sidebar.css";
 
 interface Props {
@@ -21,7 +21,15 @@ const Sidebar = ({ children }: Props) => {
       <div className="sidebar">
         <div className="logo_content">
           <div className="logo">
-            <div className="logo_name">Ajarin</div>
+            <div className="logo_name">
+              <h1
+                className="fw-bold"
+                style={{ color: "#fff", fontSize: "36px" }}
+              >
+                ajar
+                <span style={{ color: "#F6ECA9" }}>in</span>
+              </h1>
+            </div>
           </div>
           <i id="btn" onClick={addElement}>
             <MdMenu />
@@ -30,28 +38,36 @@ const Sidebar = ({ children }: Props) => {
         <ul className="">
           <li>
             <a href="#">
-              <i><GoHome /></i>
+              <i>
+                <GoHome />
+              </i>
               <span className="links_name">Dashboard</span>
             </a>
             <span className="tooltip">Dashboard</span>
           </li>
           <li>
             <a href="#">
-              <i><HiOutlineBookOpen /></i>
+              <i>
+                <HiOutlineBookOpen />
+              </i>
               <span className="links_name">Discussion</span>
             </a>
             <span className="tooltip">Discussion</span>
           </li>
           <li>
             <a href="#">
-              <i><PiEyeglassesLight /></i>
+              <i>
+                <PiEyeglassesLight />
+              </i>
               <span className="links_name">Lecturer</span>
             </a>
             <span className="tooltip">Lecturer</span>
           </li>
           <li>
             <a href="#">
-              <i><IoChatboxEllipsesOutline /></i>
+              <i>
+                <IoChatboxEllipsesOutline />
+              </i>
               <span className="links_name">Forum</span>
             </a>
             <span className="tooltip">Forum</span>
