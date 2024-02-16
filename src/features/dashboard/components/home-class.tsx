e487dd513acf38b300d954c41ca81206f6e6f672
Turic <ivanderglad200@gmail.com>
2024-02-17 00:10:47 +0700

    change file to another component

---
+++ src/features/dashboard/components/home-class.tsx
@@ -0,0 +1,170 @@
+import { useEffect, useState } from "react";
+import axios from "../../../api/axios";
+import {
+  CourseListOutput,
+  CourseListSchema,
+  transfromToCourseListOutput,
+} from "../../../model/course/course-list";
+import { ApiResponse } from "../../../model/schema/base_schema";
+import { Pagination } from "../../../shared";
+
+const COURSE_URL = "/api/course";
+
+function HomeClass() {
+  const [courseList, setCourseList] = useState<CourseListOutput>({
+    courseList: [],
+  });
+  const [currentPageCourse, setCurrentPageCourse] = useState(1);
+  const [classPerPageCourse, setClassPerPageCourse] = useState(4);
+
+  const lastCourseIndex = currentPageCourse * classPerPageCourse;
+  const firstCourseIndex = lastCourseIndex - classPerPageCourse;
+  const currentCourse = courseList.courseList.slice(
+    firstCourseIndex,
+    lastCourseIndex
+  );
+
+  function handlePageChangeCourse(value: any) {
+    if (value === "&laquo;" || value === "... ") {
+      setCurrentPageCourse(1);
+    } else if (value === "&lsaquo;") {
+      if (currentPageCourse !== 1) {
+        setCurrentPageCourse(currentPageCourse - 1);
+      }
+    } else if (value === "&rsaquo;") {
+      if (
+        currentPageCourse !==
+        Math.ceil(courseList.courseList.length / classPerPageCourse)
+      ) {
+        setCurrentPageCourse(currentPageCourse + 1);
+      }
+    } else if (value === "&raquo;" || value === " ...") {
+      setCurrentPageCourse(
+        Math.ceil(courseList.courseList.length / classPerPageCourse)
+      );
+    } else {
+      setCurrentPageCourse(value);
+    }
+  }
+
+  const fetchDataCourse = async () => {
+    try {
+      const response = await axios.get<ApiResponse<CourseListSchema>>(
+        COURSE_URL,
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(response);
+      setCourseList(transfromToCourseListOutput(response.data.outputSchema));
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchDataCourse();
+  }, []);
+
+  return (
+    <>
+      <div className="class-wrapper">
+        <div className="card" style={{ backgroundColor: "#11235a" }}>
+          <div className="card-body">
+            <div className="row">
+              {currentCourse.map((data, index) => (
+                <div
+                  className="col-md-3 d-flex align-items-stretch mb-3"
+                  key={index}
+                >
+                  <div className="card" style={{ border: "none" }}>
+                    <div className="container-class-header">
+                      <img
+                        className="disc-image img-fluid"
+                        src={`assets/${data.image}`}
+                        alt=""
+                        style={{ height: "20rem" }}
+                      />
+
+                      <div className="top-left p-1">
+                        <img
+                          className="img-fluid"
+                          src={`assets/coin.png`}
+                          alt=""
+                          style={{ height: "24px" }}
+                        />
+                        <span style={{ marginLeft: "5px" }}>{data.price}</span>
+                      </div>
+                      <div className="top-right" style={{ fontSize: "14px" }}>
+                        {data.chapter} chapter
+                      </div>
+                    </div>
+                    <div className="card-body p-2">
+                      <div className="card-text">
+                        <div className="class-content">
+                          <div className="class">
+                            <div className="d-block">
+                              <div
+                                className=""
+                                style={{
+                                  display: "flex",
+                                  alignItems: "center",
+                                }}
+                              >
+                                <img
+                                  src={`assets/coin.png`}
+                                  alt="abc"
+                                  className="me-2"
+                                />
+                                <span
+                                  style={{
+                                    color: "#000",
+                                    alignItems: "center",
+                                  }}
+                                >
+                                  Pengajar
+                                </span>
+                              </div>
+                              <div className="d-block p-1">
+                                <div className="title-class mb-2">
+                                  <h3 style={{ color: "#000" }}>
+                                    {data.title}
+                                  </h3>
+                                </div>
+                                {data.category.map((category, index) => (
+                                  <span
+                                    className="badge badge-outlined me-2"
+                                    style={{
+                                      borderColor: "#000",
+                                      backgroundColor: "#fff",
+                                      color: "#000",
+                                      borderRadius: "15px",
+                                    }}
+                                    key={index}
+                                  >
+                                    {category}
+                                  </span>
+                                ))}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <Pagination
+            totalClass={courseList.courseList.length}
+            classPerPage={classPerPageCourse}
+            onPageChange={handlePageChangeCourse}
+            currentPage={currentPageCourse}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomeClass;
---
+++ src/features/dashboard/components/home-discussion.tsx
@@ -0,0 +1,187 @@
+import { useEffect, useState } from "react";
+import axios from "../../../api/axios";
+import {
+    ClassList,
+  DiscussionListOutput,
+  DiscussionListSchema,
+  transfromToDiscussionListOutput,
+} from "../../../model/course/course-list";
+import { ApiResponse } from "../../../model/schema/base_schema";
+import { ModalCentered, Pagination } from "../../../shared";
+
+const CLASS_URL = "/api/discussion";
+
+function HomeDiscussion() {
+  const [classList, setClassList] = useState<DiscussionListOutput>({
+    classList: [],
+  });
+
+  const [classData, setClassData] = useState<ClassList>({
+    title: "",
+    category: [],
+    date: new Date(),
+    description: "",
+    endtime: new Date(),
+    image: "",
+    maxPeople: "",
+    price: "",
+    starttime: new Date(),
+  });
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [classPerPage, setClassPerPage] = useState(8);
+  const [showModal, setShowModal] = useState(false);
+
+  const lastClassIndex = currentPage * classPerPage;
+  const firstClassIndex = lastClassIndex - classPerPage;
+  const currentClass = classList.classList.slice(
+    firstClassIndex,
+    lastClassIndex
+  );
+
+  function handlePageChange(value: any) {
+    if (value === "&laquo;" || value === "... ") {
+      setCurrentPage(1);
+    } else if (value === "&lsaquo;") {
+      if (currentPage !== 1) {
+        setCurrentPage(currentPage - 1);
+      }
+    } else if (value === "&rsaquo;") {
+      if (
+        currentPage !== Math.ceil(classList.classList.length / classPerPage)
+      ) {
+        setCurrentPage(currentPage + 1);
+      }
+    } else if (value === "&raquo;" || value === " ...") {
+      setCurrentPage(Math.ceil(classList.classList.length / classPerPage));
+    } else {
+      setCurrentPage(value);
+    }
+  }
+
+  const fetchDataDiscussion = async () => {
+    try {
+      const response = await axios.get<ApiResponse<DiscussionListSchema>>(
+        CLASS_URL,
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(response);
+      setClassList(transfromToDiscussionListOutput(response.data.outputSchema));
+    } catch (error) {}
+  };
+
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = (data: ClassList) => {
+    setClassData({
+      ...classData,
+      category: data.category,
+      date: data.date,
+      description: data.description,
+      endtime: data.endtime,
+      image: data.image,
+      maxPeople: data.maxPeople,
+      price: data.price,
+      starttime: data.starttime,
+      title: data.title,
+    });
+    setShowModal(true);
+  };
+
+  useEffect(() => {
+    fetchDataDiscussion();
+  }, []);
+  return (
+    <>
+      <div className="class-wrapper">
+        <div className="card" style={{ backgroundColor: "#11235a" }}>
+          <div className="card-body">
+            <div className="row">
+              {currentClass.map((data, index) => (
+                <div
+                  className="col-md-3 d-flex align-items-stretch mb-3"
+                  key={index}
+                  onClick={() => handleShowModal(data)}
+                >
+                  <div
+                    className="card"
+                    style={{
+                      background: "transparent",
+                      border: "none",
+                    }}
+                  >
+                    <div className="container-class-header">
+                      <img
+                        className="disc-image img-fluid"
+                        src={`assets/${data.image}`}
+                        alt=""
+                        style={{ height: "10rem" }}
+                      />
+                      <div className="bottom-left">{data.date.toString()}</div>
+                      <div className="top-left p-1">
+                        <img
+                          className="img-fluid"
+                          src={`assets/coin.png`}
+                          alt=""
+                          style={{ height: "24px" }}
+                        />
+                        <span style={{ marginLeft: "5px" }}>{data.price}</span>
+                      </div>
+                      <div className="top-right">Top Right</div>
+                      <div className="bottom-right">
+                        {data.starttime.toString()} - {data.endtime.toString()}
+                      </div>
+                    </div>
+                    <div className="card-body p-2">
+                      <div className="card-text">
+                        <div className="class-content">
+                          <div className="class">
+                            <div className="d-flex">
+                              <div className="me-2">
+                                <img src={`assets/coin.png`} alt="abc" />
+                              </div>
+                              <div className="d-block">
+                                <div className="title-class mb-2">
+                                  <h3>{data.title}</h3>
+                                  <span>Pengajar</span>
+                                </div>
+                                {data.category.map((category, index) => (
+                                  <span
+                                    className="badge badge-outlined text-white me-2"
+                                    key={index}
+                                  >
+                                    {category}
+                                  </span>
+                                ))}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <Pagination
+            totalClass={classList.classList.length}
+            classPerPage={classPerPage}
+            onPageChange={handlePageChange}
+            currentPage={currentPage}
+          />
+        </div>
+      </div>
+
+      <ModalCentered
+        show={showModal}
+        onHide={handleCloseModal}
+        data={classData}
+      />
+    </>
+  );
+}
+
+export default HomeDiscussion;
